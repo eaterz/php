508d7db283982e2173370c2650d2ed88974b1d10
eaterz <muraskoruslans@gmail.com>
2023-11-29 10:45:51 +0200

    commit

---
+++ first php/index.php
@@ -0,0 +1,17 @@
+
+<?php
+
+
+$dzivnieki_skanas = array(
+    'kaķis' => 'mjau',
+    'suns' => 'vau',
+    'putns' => 'čiv',
+    
+);
+
+$atslegas = array_keys($dzivnieki_skanas);
+
+print_r($atslegas);
+
+
+?>
